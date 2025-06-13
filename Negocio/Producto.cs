576b25769aa872210becc2d81032634b0aa63096
Vicente Hernandez <vhernandez@consultoria-it.com>
2025-06-13 13:02:38 -0600

    carga negocio

---
+++ Negocio/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string CodigoOrigen { get; set; }
+        public int MedioDistribucion { get; set; }
+        public string Descripcion { get; set; }
+        public string ClaveProdServ { get; set; }
+        public int Tipo { get; set; }
+        public Unidad Unidad { get; set; }
+        public List<Unidad> Unidades { get; set; }
+        public List<AlmacenInfo> InfoAlmacenes { get; set; }
+        public List<Composicion> Composiciones { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Costo { get; set; }
+        public decimal PorcentajeIVA { get; set; }
+        public decimal PorcentajeIEPS { get; set; }
+        public string Moneda { get; set; }
+
+    }
+
+  
+
+}
